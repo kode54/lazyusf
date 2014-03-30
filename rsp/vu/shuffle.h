kode54/lazyusf

commit 878081a1043694da1413ef14c4f747ad09b8b607

neon stuff

rsp/vu/shuffle.h
@@ -22,65 +22,42 @@
 #define INLINE
 #endif
 
-#ifndef ARCH_MIN_SSE2
-/*
- * vector-scalar element decoding
- * Obsolete.  Consider using at least the SSE2 algorithms instead.
- */
-static const int ei[16][8] = {
-    { 00, 01, 02, 03, 04, 05, 06, 07 }, /* none (vector-only operand) */
-    { 00, 01, 02, 03, 04, 05, 06, 07 },
-    { 00, 00, 02, 02, 04, 04, 06, 06 }, /* 0Q */
-    { 01, 01, 03, 03, 05, 05, 07, 07 }, /* 1Q */
-    { 00, 00, 00, 00, 04, 04, 04, 04 }, /* 0H */
-    { 01, 01, 01, 01, 05, 05, 05, 05 }, /* 1H */
-    { 02, 02, 02, 02, 06, 06, 06, 06 }, /* 2H */
-    { 03, 03, 03, 03, 07, 07, 07, 07 }, /* 3H */
-    { 00, 00, 00, 00, 00, 00, 00, 00 }, /* 0 */
-    { 01, 01, 01, 01, 01, 01, 01, 01 }, /* 1 */
-    { 02, 02, 02, 02, 02, 02, 02, 02 }, /* 2 */
-    { 03, 03, 03, 03, 03, 03, 03, 03 }, /* 3 */
-    { 04, 04, 04, 04, 04, 04, 04, 04 }, /* 4 */
-    { 05, 05, 05, 05, 05, 05, 05, 05 }, /* 5 */
-    { 06, 06, 06, 06, 06, 06, 06, 06 }, /* 6 */
-    { 07, 07, 07, 07, 07, 07, 07, 07 }  /* 7 */
+#ifdef ARCH_MIN_ARM_NEON
+static const unsigned char smask[16][16] = {
+    {0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF},
+    {0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF},
+    {0x0,0x1,0x0,0x1,0x4,0x5,0x4,0x5,0x8,0x9,0x8,0x9,0xC,0xD,0xC,0xD},
+    {0x2,0x3,0x2,0x3,0x6,0x7,0x6,0x7,0xA,0xB,0xA,0xB,0xE,0xF,0xE,0xF},
+    {0x0,0x1,0x0,0x1,0x0,0x1,0x0,0x1,0x8,0x9,0x8,0x9,0x8,0x9,0x8,0x9},
+    {0x2,0x3,0x2,0x3,0x2,0x3,0x2,0x3,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB},
+    {0x4,0x5,0x4,0x5,0x4,0x5,0x4,0x5,0xC,0xD,0xC,0xD,0xC,0xD,0xC,0xD},
+    {0x6,0x7,0x6,0x7,0x6,0x7,0x6,0x7,0xE,0xF,0xE,0xF,0xE,0xF,0xE,0xF},
+    {0x0,0x1,0x0,0x1,0x0,0x1,0x0,0x1,0x0,0x1,0x0,0x1,0x0,0x1,0x0,0x1},
+    {0x2,0x3,0x2,0x3,0x2,0x3,0x2,0x3,0x2,0x3,0x2,0x3,0x2,0x3,0x2,0x3},
+    {0x4,0x5,0x4,0x5,0x4,0x5,0x4,0x5,0x4,0x5,0x4,0x5,0x4,0x5,0x4,0x5},
+    {0x6,0x7,0x6,0x7,0x6,0x7,0x6,0x7,0x6,0x7,0x6,0x7,0x6,0x7,0x6,0x7},
+    {0x8,0x9,0x8,0x9,0x8,0x9,0x8,0x9,0x8,0x9,0x8,0x9,0x8,0x9,0x8,0x9},
+    {0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB,0xA,0xB},
+    {0xC,0xD,0xC,0xD,0xC,0xD,0xC,0xD,0xC,0xD,0xC,0xD,0xC,0xD,0xC,0xD},
+    {0xE,0xF,0xE,0xF,0xE,0xF,0xE,0xF,0xE,0xF,0xE,0xF,0xE,0xF,0xE,0xF}
 };
 
-static const int sub_mask[16] = {
-    0x0,
-    0x0,
-    0x1, 0x1,
-    0x3, 0x3, 0x3, 0x3,
-    0x7, 0x7, 0x7, 0x7, 0x7, 0x7, 0x7, 0x7
-};
+#define conv816_to_882(v) ((int8x8x2_t) { vget_low_s8(v), vget_high_s8(v) })
 
 INLINE static void SHUFFLE_VECTOR(short* VD, short* VT, const int e)
-{
-    ALIGNED short SV[8];
-    register int i, j;
-#if (0 == 0)
-    j = sub_mask[e];
-    for (i = 0; i < N; i++)
-        SV[i] = VT[(i & ~j) | (e & j)];
-#else
-    if (e & 0x8)
-        for (i = 0; i < N; i++)
-            SV[i] = VT[(i & 0x0) | (e & 0x7)];
-    else if (e & 0x4)
-        for (i = 0; i < N; i++)
-            SV[i] = VT[(i & 0xC) | (e & 0x3)];
-    else if (e & 0x2)
-        for (i = 0; i < N; i++)
-            SV[i] = VT[(i & 0xE) | (e & 0x1)];
-    else /* if ((e == 0b0000) || (e == 0b0001)) */
-        for (i = 0; i < N; i++)
-            SV[i] = VT[(i & 0x7) | (e & 0x0)];
-#endif
-    for (i = 0; i < N; i++)
-        *(VD + i) = *(SV + i);
+{ 
+	int8x16_t xmm;
+    int8x16_t key;
+
+    xmm = vld1q_s8((const int8_t*)VT);
+    key = vld1q_s8(smask[e]);
+    xmm = vcombine_s8(vtbl2_s8(conv816_to_882(xmm), vget_low_s8(key)), vtbl2_s8(conv816_to_882(xmm), vget_high_s8(key)));
+	vst1q_s8((int8_t*)VD, xmm);
+    	    
     return;
 }
-#else
+#endif
+
 #ifdef ARCH_MIN_SSSE3
 static const unsigned char smask[16][16] = {
     {0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF},
@@ -112,11 +89,12 @@ INLINE static void SHUFFLE_VECTOR(short* VD, short* VT, const int e)
     _mm_store_si128((__m128i *)VD, xmm);
     return;
 }
-#else
+#endif
+
+#ifdef ARCH_MIN_SSSE2
 #define B(x)    ((x) & 3)
 #define SHUFFLE(a,b,c,d)    ((B(d)<<6) | (B(c)<<4) | (B(b)<<2) | (B(a)<<0))
 
-#if 0
 static const int simm[16] = {
     SHUFFLE(00, 01, 02, 03), /* vector operands */
     SHUFFLE(00, 01, 02, 03),
@@ -135,7 +113,6 @@ static const int simm[16] = {
     SHUFFLE(06, 06, 06, 06),
     SHUFFLE(07, 07, 07, 07)
 };
-#endif
 
 static __m128i shuffle_none(__m128i xmm)
 {/*
@@ -276,5 +253,65 @@ INLINE static void SHUFFLE_VECTOR(short* VD, short* VT, const int e)
     return;
 }
 #endif
+
+#if !defined ARCH_MIN_ARM_NEON && !defined ARCH_MIN_SSE2 && !defined ARCH_MIN_SSE3
+/*
+ * vector-scalar element decoding
+ * Obsolete.  Consider using at least the SSE2 algorithms instead.
+ */
+static const int ei[16][8] = {
+    { 00, 01, 02, 03, 04, 05, 06, 07 }, /* none (vector-only operand) */
+    { 00, 01, 02, 03, 04, 05, 06, 07 },
+    { 00, 00, 02, 02, 04, 04, 06, 06 }, /* 0Q */
+    { 01, 01, 03, 03, 05, 05, 07, 07 }, /* 1Q */
+    { 00, 00, 00, 00, 04, 04, 04, 04 }, /* 0H */
+    { 01, 01, 01, 01, 05, 05, 05, 05 }, /* 1H */
+    { 02, 02, 02, 02, 06, 06, 06, 06 }, /* 2H */
+    { 03, 03, 03, 03, 07, 07, 07, 07 }, /* 3H */
+    { 00, 00, 00, 00, 00, 00, 00, 00 }, /* 0 */
+    { 01, 01, 01, 01, 01, 01, 01, 01 }, /* 1 */
+    { 02, 02, 02, 02, 02, 02, 02, 02 }, /* 2 */
+    { 03, 03, 03, 03, 03, 03, 03, 03 }, /* 3 */
+    { 04, 04, 04, 04, 04, 04, 04, 04 }, /* 4 */
+    { 05, 05, 05, 05, 05, 05, 05, 05 }, /* 5 */
+    { 06, 06, 06, 06, 06, 06, 06, 06 }, /* 6 */
+    { 07, 07, 07, 07, 07, 07, 07, 07 }  /* 7 */
+};
+
+static const int sub_mask[16] = {
+    0x0,
+    0x0,
+    0x1, 0x1,
+    0x3, 0x3, 0x3, 0x3,
+    0x7, 0x7, 0x7, 0x7, 0x7, 0x7, 0x7, 0x7
+};
+
+INLINE static void SHUFFLE_VECTOR(short* VD, short* VT, const int e)
+{
+    ALIGNED short SV[8];
+    register int i, j;
+#if (0 == 0)
+    j = sub_mask[e];
+    for (i = 0; i < N; i++)
+        SV[i] = VT[(i & ~j) | (e & j)];
+#else
+    if (e & 0x8)
+        for (i = 0; i < N; i++)
+            SV[i] = VT[(i & 0x0) | (e & 0x7)];
+    else if (e & 0x4)
+        for (i = 0; i < N; i++)
+            SV[i] = VT[(i & 0xC) | (e & 0x3)];
+    else if (e & 0x2)
+        for (i = 0; i < N; i++)
+            SV[i] = VT[(i & 0xE) | (e & 0x1)];
+    else /* if ((e == 0b0000) || (e == 0b0001)) */
+        for (i = 0; i < N; i++)
+            SV[i] = VT[(i & 0x7) | (e & 0x0)];
+#endif
+    for (i = 0; i < N; i++)
+        *(VD + i) = *(SV + i);
+    return;
+}
+
 #endif
 #endif

rsp/vu/clamp.h
@@ -57,7 +57,87 @@ static INLINE void merge(short* VD, short* cmp, short* pass, short* fail)
     return;
 }
 
-#ifndef ARCH_MIN_SSE2
+#ifdef ARCH_MIN_ARM_NEON
+static INLINE void vector_copy(short * VD, short * VS)
+{
+    int16x8_t xmm;
+    xmm = vld1q_s16(VS);
+    vst1q_s16(VD, xmm);
+
+    return;
+}
+
+static INLINE void SIGNED_CLAMP_ADD(usf_state_t * state, short* VD, short* VS, short* VT)
+{
+    int16x8_t dst, src, vco, max, min;
+
+    src = vld1q_s16(VS);
+    dst = vld1q_s16(VT);
+    vco = vld1q_s16(state->co);
+
+    max = vmaxq_s16(dst, src);
+    min = vminq_s16(dst, src);
+
+    min = vaddq_s16(min, vco);
+    max = vaddq_s16(max, min);
+    vst1q_s16(VD, max);
+    return;
+}
+
+static INLINE void SIGNED_CLAMP_SUB(usf_state_t * state, short* VD, short* VS, short* VT)
+{
+    int16x8_t dst, src, vco, dif, res, xmm;
+
+    src = vld1q_s16(VS);
+    dst = vld1q_s16(VT);
+    vco = vld1q_s16(state->co);
+
+    res = vsubq_s16(src, dst);
+    dif = vaddq_s16(res, vco);
+    dif = veorq_s16(dif, res);
+    dif = vandq_s16(dif, dst);
+    xmm = vsubq_s16(src, dst);
+	   
+    src = vandq_s16(vmvnq_s16(src), dst);
+    xmm = vandq_s16(xmm, src);
+
+    xmm = vshrq_n_s16(xmm, 15);
+	
+    src = vandq_s16(vmvnq_s16(xmm), vco);
+    res = vsubq_s16(res,xmm);
+
+    vst1q_s16(VD, res);
+
+    return;
+}
+
+
+static INLINE void SIGNED_CLAMP_AM(usf_state_t * state, short* VD)
+{
+	int16x8_t pvs, pvd;
+	int16x8x2_t packed;
+	int16x8_t result;
+	int16x4_t low, high;
+	
+	pvs = vld1q_s16((const int16_t*)VACC_H);
+	pvd = vld1q_s16((const int16_t*)VACC_M);
+		
+	packed = vzipq_s16(pvd,pvs);
+		
+	low = vqmovn_s32((int32x4_t)packed.val[0]);
+	high = vqmovn_s32((int32x4_t)packed.val[1]);
+	
+	result = vcombine_s16(low,high);
+		
+	vst1q_s16(VD,result);
+	
+	return;
+}
+
+#endif
+
+#if !defined ARCH_MIN_SSE2 && !defined ARCH_MIN_ARM_NEON
+
 static INLINE void vector_copy(short* VD, short* VS)
 {
 #if (0)
@@ -92,6 +172,8 @@ static INLINE void SIGNED_CLAMP_ADD(usf_state_t * state, short* VD, short* VS, s
         VD[i] ^= 0x8000 & (hi[i] | lo[i]);
     return;
 }
+
+
 static INLINE void SIGNED_CLAMP_SUB(usf_state_t * state, short* VD, short* VS, short* VT)
 {
     ALIGNED int32_t dif[N];
@@ -113,8 +195,9 @@ static INLINE void SIGNED_CLAMP_SUB(usf_state_t * state, short* VD, short* VS, s
         VD[i] ^= 0x8000 & (hi[i] | lo[i]);
     return;
 }
+
 static INLINE void SIGNED_CLAMP_AM(usf_state_t * state, short* VD)
-{ /* typical sign-clamp of accumulator-mid (bits 31:16) */
+{ 
     ALIGNED short hi[N], lo[N];
     register int i;
 
@@ -135,7 +218,9 @@ static INLINE void SIGNED_CLAMP_AM(usf_state_t * state, short* VD)
         VD[i] ^= 0x8000 * (hi[i] | lo[i]);
     return;
 }
-#else
+#endif
+
+#ifdef ARCH_MIN_SSE2
 /*
  * We actually need to write explicit SSE2 code for this because GCC 4.8.1
  * (and possibly later versions) has a code generation bug with vectorizing

rsp/vu/vmulf.h
@@ -22,7 +22,8 @@
  * Wrong:  ACC(HI) = -((INT32)(acc) < 0)
  * Right:  ACC(HI) = -(SEMIFRAC < 0)
  */
-#define SEMIFRAC    (VS[i]*VT[i]*2/2 + 0x8000/2)
+//#define SEMIFRAC    (VS[i]*VT[i]*2/2 + 0x8000/2)
+#define SEMIFRAC    (VS[i]*VT[i]*2/2 + 0x4000)
 #endif
 
 INLINE static void do_mulf(usf_state_t * state, short* VD, short* VS, short* VT)
@@ -35,7 +36,7 @@ INLINE static void do_mulf(usf_state_t * state, short* VD, short* VS, short* VT)
         VACC_M[i] = (SEMIFRAC << 1) >> 16;
     for (i = 0; i < N; i++)
         VACC_H[i] = -((VACC_M[i] < 0) & (VS[i] != VT[i])); /* -32768 * -32768 */
-#ifndef ARCH_MIN_SSE2
+#if !defined ARCH_MIN_SSE2 && !defined ARCH_MIN_ARM_NEON
     vector_copy(VD, VACC_M);
     for (i = 0; i < N; i++)
         VD[i] -= (VACC_M[i] < 0) & (VS[i] == VT[i]); /* ACC b 31 set, min*min */

commit 73a8281c149d7663a4823934b67c5bfaa0f0f71a

Fix regression introduced in commit 878081a.

rsp/vu/shuffle.h
@@ -91,7 +91,7 @@ INLINE static void SHUFFLE_VECTOR(short* VD, short* VT, const int e)
 }
 #endif
 
-#ifdef ARCH_MIN_SSSE2
+#ifdef ARCH_MIN_SSE2
 #define B(x)    ((x) & 3)
 #define SHUFFLE(a,b,c,d)    ((B(d)<<6) | (B(c)<<4) | (B(b)<<2) | (B(a)<<0))
 
@@ -254,7 +254,7 @@ INLINE static void SHUFFLE_VECTOR(short* VD, short* VT, const int e)
 }
 #endif
 
-#if !defined ARCH_MIN_ARM_NEON && !defined ARCH_MIN_SSE2 && !defined ARCH_MIN_SSE3
+#if !defined ARCH_MIN_ARM_NEON && !defined ARCH_MIN_SSE2 && !defined ARCH_MIN_SSSE3
 /*
  * vector-scalar element decoding
  * Obsolete.  Consider using at least the SSE2 algorithms instead.