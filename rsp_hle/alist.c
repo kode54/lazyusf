kode54/lazyusf

commit 458a456ac24b1ae0ae5fd2823c654b0b0be74d19

Fixed MSVC project file, and removed C99-isms for MSVC

rsp_hle/alist.c
@@ -21,7 +21,11 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+#ifndef _MSC_VER
 #include <stdbool.h>
+#else
+#include "mystdbool.h"
+#endif
 #include <stdint.h>
 #include <string.h>
 
@@ -67,11 +71,13 @@ static void alist_envmix_mix(size_t n, int16_t** dst, const int16_t* gains, int1
 
 static int16_t ramp_step(struct ramp_t* ramp)
 {
+	bool target_reached;
+
     ramp->value += ramp->step;
 
-    bool target_reached = (ramp->step <= 0)
-        ? (ramp->value <= ramp->target)
-        : (ramp->value >= ramp->target);
+    target_reached = (ramp->step <= 0)
+    ? (ramp->value <= ramp->target)
+    : (ramp->value >= ramp->target);
 
     if (target_reached)
     {
@@ -79,7 +85,7 @@ static int16_t ramp_step(struct ramp_t* ramp)
         ramp->step  = 0;
     }
 
-    return (ramp->value >> 16);
+    return (int16_t)(ramp->value >> 16);
 }
 
 /* global functions */
@@ -332,14 +338,14 @@ void alist_envmix_exp(
 
     *(int16_t *)(save_buffer +  0) = wet;               /* 0-1 */
     *(int16_t *)(save_buffer +  2) = dry;               /* 2-3 */
-    *(int32_t *)(save_buffer +  4) = ramps[0].target;   /* 4-5 */
-    *(int32_t *)(save_buffer +  6) = ramps[1].target;   /* 6-7 */
+    *(int32_t *)(save_buffer +  4) = (int32_t)ramps[0].target;   /* 4-5 */
+    *(int32_t *)(save_buffer +  6) = (int32_t)ramps[1].target;   /* 6-7 */
     *(int32_t *)(save_buffer +  8) = exp_rates[0];      /* 8-9 (save_buffer is a 16bit pointer) */
     *(int32_t *)(save_buffer + 10) = exp_rates[1];      /* 10-11 */
     *(int32_t *)(save_buffer + 12) = exp_seq[0];        /* 12-13 */
     *(int32_t *)(save_buffer + 14) = exp_seq[1];        /* 14-15 */
-    *(int32_t *)(save_buffer + 16) = ramps[0].value;    /* 12-13 */
-    *(int32_t *)(save_buffer + 18) = ramps[1].value;    /* 14-15 */
+    *(int32_t *)(save_buffer + 16) = (int32_t)ramps[0].value;    /* 12-13 */
+    *(int32_t *)(save_buffer + 18) = (int32_t)ramps[1].value;    /* 14-15 */
     memcpy(hle->dram + address, (uint8_t *)save_buffer, 80);
 }
 
@@ -407,12 +413,12 @@ void alist_envmix_lin(
 
     *(int16_t *)(save_buffer +  0) = wet;            /* 0-1 */
     *(int16_t *)(save_buffer +  2) = dry;            /* 2-3 */
-    *(int16_t *)(save_buffer +  4) = ramps[0].target >> 16; /* 4-5 */
-    *(int16_t *)(save_buffer +  6) = ramps[1].target >> 16; /* 6-7 */
-    *(int32_t *)(save_buffer +  8) = ramps[0].step;  /* 8-9 (save_buffer is a 16bit pointer) */
-    *(int32_t *)(save_buffer + 10) = ramps[1].step;  /* 10-11 */
-    *(int32_t *)(save_buffer + 16) = ramps[0].value; /* 16-17 */
-    *(int32_t *)(save_buffer + 18) = ramps[1].value; /* 18-19 */
+    *(int16_t *)(save_buffer +  4) = (int16_t)ramps[0].target >> 16; /* 4-5 */
+    *(int16_t *)(save_buffer +  6) = (int16_t)ramps[1].target >> 16; /* 6-7 */
+    *(int32_t *)(save_buffer +  8) = (int32_t)ramps[0].step;  /* 8-9 (save_buffer is a 16bit pointer) */
+    *(int32_t *)(save_buffer + 10) = (int32_t)ramps[1].step;  /* 10-11 */
+    *(int32_t *)(save_buffer + 16) = (int32_t)ramps[0].value; /* 16-17 */
+    *(int32_t *)(save_buffer + 18) = (int32_t)ramps[1].value; /* 18-19 */
     memcpy(hle->dram + address, (uint8_t *)save_buffer, 80);
 }
 
@@ -429,14 +435,14 @@ void alist_envmix_nead(
         uint16_t *env_steps,
         const int16_t *xors)
 {
-    /* make sure count is a multiple of 8 */
-    count = align(count, 8);
-
     int16_t *in = (int16_t*)(hle->alist_buffer + dmemi);
     int16_t *dl = (int16_t*)(hle->alist_buffer + dmem_dl);
     int16_t *dr = (int16_t*)(hle->alist_buffer + dmem_dr);
     int16_t *wl = (int16_t*)(hle->alist_buffer + dmem_wl);
     int16_t *wr = (int16_t*)(hle->alist_buffer + dmem_wr);
+
+    /* make sure count is a multiple of 8 */
+    count = align(count, 8);
 
     if (swap_wet_LR)
         swap(&wl, &wr);
@@ -660,10 +666,14 @@ void alist_adpcm(
         uint32_t loop_address,
         uint32_t last_frame_address)
 {
-    assert((count & 0x1f) == 0);
-
     int16_t last_frame[16];
     size_t i;
+
+    adpcm_predict_frame_t predict_frame = (two_bit_per_sample)
+        ? adpcm_predict_frame_2bits
+        : adpcm_predict_frame_4bits;
+
+    assert((count & 0x1f) == 0);
 
     if (init)
         memset(last_frame, 0, 16*sizeof(last_frame[0]));
@@ -672,10 +682,6 @@ void alist_adpcm(
 
     for(i = 0; i < 16; ++i, dmemo += 2)
         *(int16_t*)(hle->alist_buffer + (dmemo ^ S16)) = last_frame[i];
-
-    adpcm_predict_frame_t predict_frame = (two_bit_per_sample)
-        ? adpcm_predict_frame_2bits
-        : adpcm_predict_frame_4bits;
 
     while (count != 0) {
         int16_t frame[16];

prj/msvc/lazyusf.vcxproj
@@ -25,6 +25,24 @@
       <PreprocessorDefinitions Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">WIN32;ARCH_MIN_SSE2;_DEBUG;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <PreprocessorDefinitions Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">WIN32;ARCH_MIN_SSE2;NDEBUG;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>
     </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\alist.c" />
+    <ClCompile Include="..\..\rsp_hle\alist_audio.c" />
+    <ClCompile Include="..\..\rsp_hle\alist_naudio.c" />
+    <ClCompile Include="..\..\rsp_hle\alist_nead.c" />
+    <ClCompile Include="..\..\rsp_hle\audio.c">
+      <ObjectFileName Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">$(IntDir)/audio_hle.obj</ObjectFileName>
+      <ObjectFileName Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">$(IntDir)/audio_hle.obj</ObjectFileName>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\cicx105.c" />
+    <ClCompile Include="..\..\rsp_hle\hle.c" />
+    <ClCompile Include="..\..\rsp_hle\jpeg.c" />
+    <ClCompile Include="..\..\rsp_hle\memory.c">
+      <ObjectFileName Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">$(IntDir)/memory_hle.obj</ObjectFileName>
+      <ObjectFileName Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">$(IntDir)/memory_hle.obj</ObjectFileName>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\mp3.c" />
+    <ClCompile Include="..\..\rsp_hle\musyx.c" />
+    <ClCompile Include="..\..\rsp_hle\plugin.c" />
     <ClCompile Include="..\..\tlb.c" />
     <ClCompile Include="..\..\usf.c" />
   </ItemGroup>
@@ -92,6 +110,17 @@
     <ClInclude Include="..\..\rsp\vu\vsubc.h" />
     <ClInclude Include="..\..\rsp\vu\vu.h" />
     <ClInclude Include="..\..\rsp\vu\vxor.h" />
+    <ClInclude Include="..\..\rsp_hle\alist.h" />
+    <ClInclude Include="..\..\rsp_hle\alist_internal.h" />
+    <ClInclude Include="..\..\rsp_hle\arithmetics.h" />
+    <ClInclude Include="..\..\rsp_hle\audio.h" />
+    <ClInclude Include="..\..\rsp_hle\cicx105.h" />
+    <ClInclude Include="..\..\rsp_hle\hle.h" />
+    <ClInclude Include="..\..\rsp_hle\hle_external.h" />
+    <ClInclude Include="..\..\rsp_hle\hle_internal.h" />
+    <ClInclude Include="..\..\rsp_hle\jpeg.h" />
+    <ClInclude Include="..\..\rsp_hle\memory.h" />
+    <ClInclude Include="..\..\rsp_hle\musyx.h" />
     <ClInclude Include="..\..\tlb.h" />
     <ClInclude Include="..\..\types.h" />
     <ClInclude Include="..\..\usf.h" />

prj/msvc/lazyusf.vcxproj.filters
@@ -21,6 +21,15 @@
     <Filter Include="rsp\Header Files\vu">
       <UniqueIdentifier>{8cfbae89-d3d4-41d5-87a9-fbccc3f6def5}</UniqueIdentifier>
     </Filter>
+    <Filter Include="rsp_hle">
+      <UniqueIdentifier>{4dff417c-1d61-4a46-8adf-da21fc87458e}</UniqueIdentifier>
+    </Filter>
+    <Filter Include="rsp_hle\Source Files">
+      <UniqueIdentifier>{6c5fbcb6-5341-4bfb-8541-9532f610a3df}</UniqueIdentifier>
+    </Filter>
+    <Filter Include="rsp_hle\Header Files">
+      <UniqueIdentifier>{2545fc45-81cf-4cf1-924b-0d83e1ea5093}</UniqueIdentifier>
+    </Filter>
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="..\..\audio.c">
@@ -61,6 +70,42 @@
     </ClCompile>
     <ClCompile Include="..\..\rsp\rsp.c">
       <Filter>rsp\Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\alist.c">
+      <Filter>rsp_hle\Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\alist_audio.c">
+      <Filter>rsp_hle\Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\alist_naudio.c">
+      <Filter>rsp_hle\Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\alist_nead.c">
+      <Filter>rsp_hle\Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\audio.c">
+      <Filter>rsp_hle\Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\cicx105.c">
+      <Filter>rsp_hle\Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\hle.c">
+      <Filter>rsp_hle\Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\jpeg.c">
+      <Filter>rsp_hle\Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\memory.c">
+      <Filter>rsp_hle\Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\mp3.c">
+      <Filter>rsp_hle\Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\musyx.c">
+      <Filter>rsp_hle\Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\rsp_hle\plugin.c">
+      <Filter>rsp_hle\Source Files</Filter>
     </ClCompile>
   </ItemGroup>
   <ItemGroup>
@@ -264,6 +309,39 @@
     </ClInclude>
     <ClInclude Include="..\..\rsp\vu\vxor.h">
       <Filter>rsp\Header Files\vu</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\rsp_hle\alist.h">
+      <Filter>rsp_hle\Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\rsp_hle\alist_internal.h">
+      <Filter>rsp_hle\Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\rsp_hle\arithmetics.h">
+      <Filter>rsp_hle\Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\rsp_hle\audio.h">
+      <Filter>rsp_hle\Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\rsp_hle\cicx105.h">
+      <Filter>rsp_hle\Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\rsp_hle\hle.h">
+      <Filter>rsp_hle\Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\rsp_hle\hle_external.h">
+      <Filter>rsp_hle\Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\rsp_hle\hle_internal.h">
+      <Filter>rsp_hle\Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\rsp_hle\jpeg.h">
+      <Filter>rsp_hle\Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\rsp_hle\memory.h">
+      <Filter>rsp_hle\Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\rsp_hle\musyx.h">
+      <Filter>rsp_hle\Header Files</Filter>
     </ClInclude>
   </ItemGroup>
 </Project>

rsp/rsp.h
@@ -15,7 +15,7 @@
 #define _RSP_H_
 
 #ifdef _MSC_VER
-#define INLINE      __inline
+#define INLINE      __forceinline
 #define NOINLINE    __declspec(noinline)
 #define ALIGNED     _declspec(align(16))
 #else

rsp/su.h
@@ -72,7 +72,7 @@ static void set_PC(usf_state_t * state, int address)
 #define WES(address)    ((address) ^ ((ENDIAN) & 00))
 #define SR_B(s, i)      (*(byte *)(((byte *)(state->SR + s)) + BES(i)))
 #define SR_S(s, i)      (*(short *)(((byte *)(state->SR + s)) + HES(i)))
-#define SE(x, b)        (-(x & (1 << b)) | (x & ~(~0 << b)))
+#define SE(x, b)        (-((signed int)x & (1 << b)) | (x & ~(~0 << b)))
 #define ZE(x, b)        (+(x & (1 << b)) | (x & ~(~0 << b)))
 
 static void ULW(usf_state_t *, int rd, uint32_t addr);

rsp_hle/alist_audio.c
@@ -21,7 +21,11 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+#ifndef _MSC_VER
 #include <stdbool.h>
+#else
+#include "mystdbool.h"
+#endif
 #include <stdint.h>
 #include <string.h>
 

rsp_hle/alist_internal.h
@@ -22,7 +22,11 @@
 #ifndef ALIST_INTERNAL_H
 #define ALIST_INTERNAL_H
 
+#ifndef _MSC_VER
 #include <stdbool.h>
+#else
+#include "mystdbool.h"
+#endif
 #include <stdint.h>
 #include <stddef.h>
 

rsp_hle/alist_naudio.c
@@ -21,7 +21,11 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+#ifndef _MSC_VER
 #include <stdbool.h>
+#else
+#include "mystdbool.h"
+#endif
 #include <stdint.h>
 
 #include "alist_internal.h"

commit 19475c5d62a82011806e01fdb2e4279648b824da

Fix regression introduced in commit 458a456.
Now Neon Genesis Evangelion should be ok.

rsp_hle/alist.c
@@ -413,8 +413,8 @@ void alist_envmix_lin(
 
     *(int16_t *)(save_buffer +  0) = wet;            /* 0-1 */
     *(int16_t *)(save_buffer +  2) = dry;            /* 2-3 */
-    *(int16_t *)(save_buffer +  4) = (int16_t)ramps[0].target >> 16; /* 4-5 */
-    *(int16_t *)(save_buffer +  6) = (int16_t)ramps[1].target >> 16; /* 6-7 */
+    *(int16_t *)(save_buffer +  4) = (int16_t)(ramps[0].target >> 16); /* 4-5 */
+    *(int16_t *)(save_buffer +  6) = (int16_t)(ramps[1].target >> 16); /* 6-7 */
     *(int32_t *)(save_buffer +  8) = (int32_t)ramps[0].step;  /* 8-9 (save_buffer is a 16bit pointer) */
     *(int32_t *)(save_buffer + 10) = (int32_t)ramps[1].step;  /* 10-11 */
     *(int32_t *)(save_buffer + 16) = (int32_t)ramps[0].value; /* 16-17 */